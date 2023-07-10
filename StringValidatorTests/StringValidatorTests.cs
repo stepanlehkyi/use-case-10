2a82f53fba7b4ebb6e2ce89033cd7c6915613107
Stepan Lehkyi <slehk@softserveinc.com>
2023-07-10 16:23:25 +0300

    UC#10 Implement regex tests

--- StringValidatorTests/StringValidatorTests.cs
+++ StringValidatorTests/StringValidatorTests.cs
@@ -5,19 +5,19 @@ namespace StringValidatorTests {
     private StringValidator _validator;
 
     [SetUp]
-    public void Setup() {
-      _validator = new StringValidator(8, @"""!\""#$%&'()*+,-./:;<=>?@[\\]^_`{|}~""");
+    public void SetUp() {
+      _validator = new StringValidator(8);
     }
 
     [Test]
     public void TestLength() {
       string tooShort = "Ab#1";
-      string tooLong = "Aaaaaaaaabbbbbbbb#1";
-      string justRight = "Abcdef#1";
+      string tooLong = "Abcdefg#1H";
+      string justRight = "AbcdE&12";
 
-      Assert.IsFalse(_validator.IsStringValid(tooShort));
-      Assert.IsFalse(_validator.IsStringValid(tooLong));
-      Assert.IsTrue(_validator.IsStringValid(justRight));
+      Assert.IsFalse(_validator.IsStringValid(tooShort), "Expected validation to fail for string shorter than 8 characters");
+      Assert.IsFalse(_validator.IsStringValid(tooLong), "Expected validation to fail for string longer than 8 characters");
+      Assert.IsTrue(_validator.IsStringValid(justRight), "Expected validation to pass for string exactly 8 characters long");
     }
 
     [Test]
@@ -27,10 +27,10 @@ namespace StringValidatorTests {
       string noSpecial = "Abcdefg1";
       string noDigit = "Abcdefg#";
 
-      Assert.IsFalse(_validator.IsStringValid(noUpper));
-      Assert.IsFalse(_validator.IsStringValid(noLower));
-      Assert.IsFalse(_validator.IsStringValid(noSpecial));
-      Assert.IsFalse(_validator.IsStringValid(noDigit));
+      Assert.IsFalse(_validator.IsStringValid(noUpper), "Expected validation to fail for string missing an uppercase letter");
+      Assert.IsFalse(_validator.IsStringValid(noLower), "Expected validation to fail for string missing a lowercase letter");
+      Assert.IsFalse(_validator.IsStringValid(noSpecial), "Expected validation to fail for string missing a special character");
+      Assert.IsFalse(_validator.IsStringValid(noDigit), "Expected validation to fail for string missing a digit");
     }
 
     [Test]
@@ -39,26 +39,27 @@ namespace StringValidatorTests {
       string trailingWhitespace = "Abcdef#1 ";
       string innerWhitespace = "Abc de#1";
 
-      Assert.IsFalse(_validator.IsStringValid(leadingWhitespace));
-      Assert.IsFalse(_validator.IsStringValid(trailingWhitespace));
-      Assert.IsFalse(_validator.IsStringValid(innerWhitespace));
+      Assert.IsFalse(_validator.IsStringValid(leadingWhitespace), "Expected validation to fail for string with leading whitespace");
+      Assert.IsFalse(_validator.IsStringValid(trailingWhitespace), "Expected validation to fail for string with trailing whitespace");
+      Assert.IsFalse(_validator.IsStringValid(innerWhitespace), "Expected validation to fail for string with inner whitespace");
     }
-
     [Test]
-    public void TestComplexCases() {
-      string mixedValidAndInvalid = "A1bc def#";
-      string repeatedCharacters = "AAAbbb#1";
-      string onlySpecialAndDigits = "#123456!";
-      string specialAtBeginning = "#Ab1cdeF";
-      string specialAtEnd = "AbcdEF1#";
-      string orderDifferent = "1#AbcdEF";
-
-      Assert.IsFalse(_validator.IsStringValid(mixedValidAndInvalid));
-      Assert.IsTrue(_validator.IsStringValid(repeatedCharacters));
-      Assert.IsFalse(_validator.IsStringValid(onlySpecialAndDigits));
-      Assert.IsTrue(_validator.IsStringValid(specialAtBeginning));
-      Assert.IsTrue(_validator.IsStringValid(specialAtEnd));
-      Assert.IsTrue(_validator.IsStringValid(orderDifferent));
+    public void AdditionalTestCases() {
+      // Inputs with various special characters
+      Assert.IsTrue(_validator.IsStringValid("Aa1!@#$%"), "Expected validation to pass for string with various special characters");
+      // Input with non-ASCII characters
+      Assert.IsFalse(_validator.IsStringValid("≈Â1#2345"), "Expected validation to fail for string with non-ASCII characters");
+      // Input with no lowercase character
+      Assert.IsFalse(_validator.IsStringValid("A1!@#$%"), "Expected validation to fail for string with no lowercase character");
+      // Input with no uppercase character
+      Assert.IsFalse(_validator.IsStringValid("a1!@#$%"), "Expected validation to fail for string with no uppercase character");
+      // Input with no digit
+      Assert.IsFalse(_validator.IsStringValid("Aa!@#$%"), "Expected validation to fail for string with no digit");
+      // Input with all lowercase characters
+      Assert.IsFalse(_validator.IsStringValid("abcdefgh"), "Expected validation to fail for string with all lowercase characters");
+      // Input with all uppercase characters
+      Assert.IsFalse(_validator.IsStringValid("ABCDEFGH"), "Expected validation to fail for string with all uppercase characters");
     }
   }
+
 }
--- use-case-10/StringValidator.cs
+++ use-case-10/StringValidator.cs
@@ -4,14 +4,14 @@ namespace use_case_10 {
   public class StringValidator {
     private readonly Regex _validator;
 
-    public StringValidator(uint length, string specialChars) {
-      string escapedSpecialChars = Regex.Escape(specialChars);
-      string pattern = $"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[{escapedSpecialChars}])(?!.*\\s).{{1,{length}}}$";
-      _validator = new Regex(pattern, RegexOptions.Compiled);
+    public StringValidator(uint length) {
+      string specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\\\\";
+      string pattern = $"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[{specialChars}])[^\\s]{{{length},{length}}}$";
+      _validator = new Regex(pattern);
     }
 
     public bool IsStringValid(string inputString) {
       return _validator.IsMatch(inputString);
     }
   }
-  }
+}
